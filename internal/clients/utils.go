e4fbdf4b97b1aa78663b3a86dc6fa338ce6c810d
theodorsjetnanutvik <theodor.utvik@gmail.com>
2026-03-21 19:34:28 +0100

    refactor: move mean() to shared utils.go and add doc

---
+++ internal/clients/utils.go
@@ -0,0 +1,13 @@
+package clients
+
+// mean returns the arithmetic mean of vals, or 0 if vals is empty.
+func mean(vals []float64) float64 {
+	if len(vals) == 0 {
+		return 0
+	}
+	var sum float64
+	for _, v := range vals {
+		sum += v
+	}
+	return sum / float64(len(vals))
+}
